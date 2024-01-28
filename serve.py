fb016f7c32aa9c2427f66a4be9c3cd7ff1b1d364
manylayers <domatthew1854@gmail.com>
2024-01-28 13:58:00 -0800

    fixed python3 stress test

--- serve.py
+++ serve.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route("/", methods=['GET', 'POST'])
 def num():
     if request.method == 'POST':
-        command = ["python", "stress_cpu.py"]
+        command = ["python3", "stress_cpu.py"]
         process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         stdout, stderr = process.communicate()
         return "Output: {}\nError: {}".format(stdout, stderr)
